c9a624569e9c1ccba1b849e1f8353ead3ad49ec0
lucaskimjs <96285508+lucaskijs@users.noreply.github.com>
2024-01-26 17:53:37 +0900

    Update 윷놀이 (2490).c

--- 윷놀이 (2490).c
+++ 윷놀이 (2490).c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+int main()
+{
+  int a, b, c, d, i;
+  char result[3];
+
+  for (i=0; i<3; i++)
+    {
+      scanf("%d %d %d %d", &a, &b, &c, &d);
+      if(a+b+c+d == 3)
+      {
+        result[i] = 'A';
+      }
+      else if(a+b+c+d == 2)
+      {
+        result[i] = 'B';
+      }
+      else if(a+b+c+d == 1)
+      {
+        result[i] = 'C';
+      }
+      else if(a+b+c+d == 0)
+      {
+        result[i] = 'D';
+      }
+      else if(a+b+c+d == 4)
+      {
+        result[i] = 'E';
+      }
+    }
+
+  printf("%c\n", result[0]);
+  printf("%c\n", result[1]);
+  printf("%c", result[2]);
+  
+  return 0;
+}
