e0eed5975b8cd74208dcfb442836a5eee2183294
lucaskimjs <96285508+lucaskijs@users.noreply.github.com>
2024-01-28 01:22:06 +0900

    Create A+B - 4 (10951).c

---
+++ A+B - 4 (10951).c
@@ -0,0 +1,14 @@
+//어려움
+#include <stdio.h>
+int main() {
+
+  int a, b;
+
+  while (1) {
+    if (scanf("%d %d", &a, &b) == EOF) {
+      break;
+    }
+    printf("%d\n", a + b);
+  }
+  return 0;
+}
