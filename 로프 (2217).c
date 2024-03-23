4bd87d8f0ef685ca3b5160795456c1685f497de0
lucaskimjs <96285508+lucaskijs@users.noreply.github.com>
2024-03-23 13:57:58 +0900

    Create 로프 (2217).c

---
+++ 로프 (2217).c
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <cstring>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+  int N, i, j, arr[100001], max=0;
+  scanf("%d", &N);
+
+  int a;
+  for(i=0; i<N; i++)
+    {
+      scanf("%d", &a);
+      arr[i]=a;
+    }
+
+  sort(arr, arr+N);
+
+  for(i=0; i<N; i++)
+    {
+      if(max < arr[i] * (N-i))
+        {
+          max = arr[i] * (N-i);
+        } 
+    }
+
+  printf("%d", max);
+  return 0;
+}
