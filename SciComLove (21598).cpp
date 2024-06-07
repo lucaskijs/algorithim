f2b0c3633cc0be86341be87f6a199cdb3e36ac4e
lucaskimjs <96285508+lucaskijs@users.noreply.github.com>
2024-06-07 15:07:08 +0900

    Create SciComLove (21598).cpp

---
+++ SciComLove (21598).cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    if(n==1)
+    {
+        printf("SciComLove");
+    }
+    else
+    {
+        printf("SciComLove\nSciComLove");
+    }
+}
