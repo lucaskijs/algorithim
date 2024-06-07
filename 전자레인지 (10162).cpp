2eef4fc559ecf2230edb82b4d709aac158e708b1
lucaskimjs <96285508+lucaskijs@users.noreply.github.com>
2024-06-07 16:42:49 +0900

    Create 전자레인지 (10162).cpp

---
+++ 전자레인지 (10162).cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+  int t, a = 0, b = 0, c = 0;
+  scanf("%d", &t);
+  while (t >= 300) {
+    t -= 300;
+    a++;
+  }
+  while (t >= 60) {
+    t -= 60;
+    b++;
+  }
+  while (t > 0) {
+    t -= 10;
+    c++;
+  }
+  if (t != 0) {
+    printf("-1");
+  } else {
+    printf("%d %d %d", a, b, c);
+  }
+
+  return 0;
+}
