f322051688b35331405773c97c5701bc55a0ef80
lucaskimjs <96285508+lucaskijs@users.noreply.github.com>
2024-06-07 16:13:55 +0900

    Create SciComLove (2024) (31746).cpp

---
+++ SciComLove (2024) (31746).cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+  long long n;
+  scanf("%lld", &n);
+
+  if(n%2 == 1)
+  {
+    printf("evoLmoCicS");
+  }
+  else
+  {
+    printf("SciComLove");
+  }
+
+  return 0;
+}
